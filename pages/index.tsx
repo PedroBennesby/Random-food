a6c93d931b0be542c4249138f8a5ae379a1277c4
Pedro Bennesby <pedrobennesby@gmail.com>
2021-08-10 20:06:20 -0300

    feat: Adicionado mudança de tema + fix TS #2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,54 @@
 import Head from 'next/head'
+import {
+  Container,
+  Text,
+  useColorMode,
+  Button,
+  IconButton,
+  Switch,
+} from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 export default function Home() {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Homepage</title>
+        <title>Food Randomizer</title>
       </Head>
 
-      <main>
-        <h1>Homepage</h1>
-      </main>
-    </div>
+      <Container
+        my="40px"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>Teste color mode</Text>
+
+        {colorMode === 'light' ? (
+          <IconButton
+            my="20px"
+            isRound
+            color="gray.800"
+            icon={<MoonIcon />}
+            variant="outline"
+            onClick={toggleColorMode}
+            aria-label="Change color mode"
+          />
+        ) : (
+          <IconButton
+            my="20px"
+            isRound
+            color="yellow.500"
+            icon={<SunIcon />}
+            variant="outline"
+            onClick={toggleColorMode}
+            aria-label="Change color mode"
+          />
+        )}
+      </Container>
+    </>
   )
 }
